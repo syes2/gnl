53e4edeccb8e8b8d7444b90d5bd241caae335b87
Seungyeon Baek <seungbae@c10r7s14.42seoul.kr>
2022-10-13 16:16:31 +0900

    fianl

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: seungbae <seungbae@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 17:09:28 by seungbae          #+#    #+#             */
-/*   Updated: 2022/10/12 17:32:03 by seungbae         ###   ########seoul.kr  */
+/*   Updated: 2022/10/13 13:15:17 by seungbae         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,28 +44,28 @@ size_t	ft_strlen(const char *s)
 
 char	*ft_strjoin(char *s1, char *s2)
 {
-	char			*str;
-	unsigned int	i;
-	unsigned int	j;
+	char	*res;
+	int		i;
+	int		j;
 
 	i = 0;
 	j = 0;
 	if (!s1 && !s2)
 		return (NULL);
-	str = malloc(((ft_strlen(s1) + ft_strlen(s2)) + 1) * sizeof(char));
-	if (!str)
+	res = malloc((sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)) + 1));
+	if (!res)
 		return (NULL);
 	if (s1)
 	{
 		while (s1[i])
 		{
-			str[i] = s1[i];
+			res[i] = s1[i];
 			i++;
 		}
 	}
 	while (s2[j])
-		str[i++] = s2[j++];
+		res[i++] = s2[j++];
 	free (s1);
-	str[i] = '\0';
-	return (str);
+	res[i] = '\0';
+	return (res);
 }
